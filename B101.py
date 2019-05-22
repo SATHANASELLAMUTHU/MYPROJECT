083c1cc8fdd85561d6f288235e8d3d3cf9ef6626
SATHANASELLAMUTHU <49227033+SATHANASELLAMUTHU@users.noreply.github.com>
2019-05-22 19:23:19 +0530

    B101.py

--- B101.py
+++ B101.py
@@ -1,4 +1,4 @@
-n1=int(input("Enter the number n1:"))
-n2=int(input("Enter the number n2:"))
+num1=int(input("Enter the number n1:"))
+num2=int(input("Enter the number n2:"))
 ldig=num1%10
 print(ldig)
